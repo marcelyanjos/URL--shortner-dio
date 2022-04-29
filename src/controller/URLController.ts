14b9af92481ca2473abe6871ed2775752480e066
Marcely Anjos <65038093+marcelyanjos@users.noreply.github.com>
2022-04-29 18:38:25 -0300

    Add files via upload

---
+++ src/controller/URLController.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+export class URLController{
+    public async shorten(req: Request, response: Response): Promise<void>{
+        // Ver se a URL já não existe
+        // Criar o hash para essa URL
+        const {origin} = req.body
+        // Salvar a URL no banco
+        //Retornar a URL que a gente salvou
+    }
+}

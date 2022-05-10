4f302c5b8d8f09ab2a5bd077cf7514ce4cdd365c
Marcely Anjos <65038093+marcelyanjos@users.noreply.github.com>
2022-05-09 23:35:51 -0300

    Add files via upload
    correção de erros e finalização

--- src/controller/URLController.ts
+++ src/controller/URLController.ts
@@ -1,10 +1,31 @@
-import { Request, Response } from "express";
-export class URLController{
-    public async shorten(req: Request, response: Response): Promise<void>{
-        // Ver se a URL já não existe
-        // Criar o hash para essa URL
-        const {origin} = req.body
-        // Salvar a URL no banco
-        //Retornar a URL que a gente salvou
-    }
+import { Request, Response } from 'express'
+import shortId from 'shortid'
+import { config } from '../config/Constants'
+import { URLModel } from '../database/model/URL'
+
+export class URLController {
+	public async shorten(req: Request, response: Response): Promise<void> {
+		const { originURL } = req.body
+		const url = await URLModel.findOne({ originURL })
+		if (url) {
+			response.json(url)
+			return
+		}
+		const hash = shortId.generate()
+		const shortURL = `${config.API_URL}/${hash}`
+		const newURL = await URLModel.create({ hash, shortURL, originURL })
+		response.json(newURL)
+	}
+
+	public async redirect(req: Request, response: Response): Promise<void> {
+		const { hash } = req.params
+		const url = await URLModel.findOne({ hash })
+
+		if (url) {
+			response.redirect(url.originURL)
+			return
+		}
+
+		response.status(400).json({ error: 'URL not found' })
+	}
 }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
-import express, {Request, Response} from 'express'
+import express from 'express'
+import { URLController } from './controller/URLController'
+import { MongoConnection } from './database/MongoConnection'
 
 const api = express()
+api.use(express.json())
 
-api.get('/test', (req: Request, res: Response) => {
-    res.json({success: true })
-})
-api.listen(5000, ()=> console.log('Express listening'))
+const database = new MongoConnection()
+database.connect()
+
+const urlController = new URLController()
+api.post('/shorten', urlController.shorten)
+api.get('/:hash', urlController.redirect)
+
+api.listen(5000, () => console.log('Express listening'))
